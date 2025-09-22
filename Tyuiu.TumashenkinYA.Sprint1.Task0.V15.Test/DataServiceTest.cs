0984300881c425d4bde9a440170309f878793959
Ариадна Гилёва <ariadnagileva@list.ru>
2025-09-22 22:13:52 +0500

    Task0

---
+++ Tyuiu.TumashenkinYA.Sprint1.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TumashenkinYA.Sprint1.Task0.V15.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint1.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(24, res);
+
+        }
+    }
+}
