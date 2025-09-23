d1b825325a81cc32915d02cbb79d3f3143a89b52
Ариадна Гилёва <ariadnagileva@list.ru>
2025-09-23 12:01:25 +0500

    Task2

---
+++ Tyuiu.TumashenkinYA.Sprint1.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TumashenkinYA.Sprint1.Task2.V28.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint1.Task2.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertCelsiusToKelvin(x);
+            Assert.AreEqual(275, res);
+        }
+    }
+}
---
+++ Tyuiu.TumashenkinYA.Sprint1.Task2.V28/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.TumashenkinYA.Sprint1.Task2.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #1 | Выполнил: Тумашенкин Ярослав Анатольевич | ПКТб-25-1";
+        //Длинна строки 75 символов
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Арифметические операторы в C#                                           *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнил:  Тумашенкин Ярослав Анатольевич | ПКТб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*Написать программу, которая запрашивает у пользователя исходные данные,  *");
+        Console.WriteLine("*выполняет указанные расчёты и печатает результат на экране.              *");
+
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int value;
+
+        Console.WriteLine("Введите температуру в градусах Цельсия: ");
+        value = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+
+        Console.WriteLine("Температура в градусах Кельвина: " + ds.ConvertCelsiusToKelvin(value));
+        Console.ReadKey();
+    }
+}
